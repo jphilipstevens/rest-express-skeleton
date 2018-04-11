7308172bd8533000afdc0c1b6336d06f5e34f73b
Gunjan Pandya <gunjanpandya@buzz.local>
2018-04-10 21:58:02 -0400

    feat(security): Add helmet security middleware
    More info found at: https://helmetjs.github.io/

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import { getDisplayString } from "./render";
 import express from "express";
+import helmet from "helmet";
+import { getDisplayString } from "./render";
+
 console.log(getDisplayString()); // eslint-disable-line no-console
 
-const app = express()
+const app = express();
+app.use(helmet());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Server is listening on port 3000!'))
+app.listen(3000, () => console.log('Server is listening on port 3000...')) // eslint-disable-line no-console
